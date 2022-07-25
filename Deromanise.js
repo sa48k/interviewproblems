afc243a821e3290f9cd39913e4138c7c28d129cc
sa48k <sa48k@users.noreply.github.com>
2022-07-25 23:24:23 +1200

    Update Deromanise.js
    destructuring for readability

--- Deromanise.js
+++ Deromanise.js
@@ -9,14 +9,15 @@ let count = 0;
 function romanToInt(s) {
     // deal with these six special cases where subtraction is used
     const specials = [['IV', 4], ['IX', 9], ['XL', 40], ['XC', 90], ['CD', 400], ['CM', 900]];
-    for (let i=0; i<specials.length; i++) {
-        if (~s.indexOf(specials[i][0])) {       // if any special numerals are found in the string,
-            count += specials[i][1]             // add their value to count,
-            s = s.replace(specials[i][0], '')   // then remove them 
+    for (let i = 0; i < specials.length; i++) {
+        var [letter, value] = specials[i];
+        if (~s.indexOf(letter)) {       // if any special numerals are found in the string,
+            count += value             // add their value to count,
+            s = s.replace(letter, '')   // then remove them 
         }
     }
     
-    // deal with normal numerals
+    // deal with normal numerals // could have used a Map instead
     for (let i=0; i<s.length; i++) {
             switch (s[i]) {  
                 case 'M': { count += 1000; break;}
