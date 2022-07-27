cb7f707876aa2ebe81549b5a07bf04398361aa10
sa48k <sa48k@users.noreply.github.com>
2022-07-27 23:51:45 +1200

    Update BestTimeToBuyAndSellStock.js

--- BestTimeToBuyAndSellStock.js
+++ BestTimeToBuyAndSellStock.js
@@ -1,4 +1,3 @@
-
 // Naive solution - runs in O(n^2) time
 // var maxProfit = function (prices) {
 //     var startday, endday, profit = 0
@@ -26,15 +25,15 @@ var maxProfit = function (prices) {
     let maxprofit = 0
     while (right < prices.length) {
         if (prices[left] < prices[right]) {             // if this trade is profitable
-            let profit = prices[right] - prices[left];  // find out HOW profitable, 
-            maxprofit = Math.max(maxprofit, profit);    // then if it's higher than the current best known profit, update the maxprofit
+            let profit = prices[right] - prices[left]  // find out HOW profitable, 
+            maxprofit = Math.max(maxprofit, profit)    // then if it's higher than the current best known profit, update the maxprofit
         } else {
-            left = right;                               // if it isn't, move the buy pointer to the sell pointer's location
+            left = right                               // if it isn't, move the buy pointer to the sell pointer's location
         }
-        right++;
+        right++
     }
-    return maxprofit;
+    return maxprofit
 };
 
-const prices = [7, 1, 4, 6, 3, 7]
+const prices = [7, 1, 4, 6, 1, 3]
 console.log(maxProfit(prices)) 
